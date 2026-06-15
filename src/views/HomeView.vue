3417462548942e2e7f6ea095bf580f8e527d2f17
DMWeggemans <daan.weggemans@student.graafschapcollege.nl>
2026-06-15 20:06:04 +0200

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -38,6 +38,18 @@
         { immediate: true }
     );
 
+    watch(
+        () => route.params.id,
+        (new_id) => {
+            const local_pokemons = JSON.parse(localStorage.getItem("pokemons") ?? "[]");
+            const pokemon = local_pokemons.find(x => x.id == new_id);
+            if (!pokemon) return;
+
+            emit('select-pokemon', pokemon);
+        },
+        { immediate: true }
+    );
+
     async function getPokemons() {
         const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1500");
         if (!response.ok) return;
